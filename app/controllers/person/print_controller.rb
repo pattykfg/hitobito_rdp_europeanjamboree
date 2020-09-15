ebe93ebdd6a493c7db4cf74f06f769e19334fec5
Peter Neubauer <development@smeky.de>
2020-09-15 22:02:23 +0200

    print preview

--- app/controllers/person/print_controller.rb
+++ app/controllers/person/print_controller.rb
@@ -1,26 +1,43 @@
-
-
 class Person::PrintController < ApplicationController
-    before_action :authorize_action
+  before_action :authorize_action
 
-    decorates :group, :person
 
-    def index 
-        @group ||= Group.find(params[:group_id])
-        @person ||= group.people.find(params[:id])
-    end
+  def index 
+    @group ||= Group.find(params[:group_id])
+    @person ||= group.people.find(params[:id])
+  end
 
-    private
-    def entry
-        @person ||= group.people.find(params[:id])
-    end
+  def preview
+    pdf = RdpEuropeanjamboree::Export::Pdf::Registration.render(@person,true)
 
-    def group
-        @group ||= Group.find(params[:group_id])
-    end
+    send_data pdf, type: :pdf, disposition: 'inline', filename: "Anmeldung-EJ-Vorschau.pdf"
+  end
 
-    def authorize_action
-        authorize!(:edit, entry)
-    end
+
+  # def submit
+  #   pdf = RdpEuropeanjamboree::Export::Pdf::Registration.render(@person,false)
+
+  #   send_data pdf, type: :pdf, disposition: 'inline', filename: "Anmeldung-EJ-" + person.id.to_s + ".pdf"
+  # end
+
+  private
+
+  def entry
+    @person ||= Person.find(params[:id])
+  end
+
+  def authorize_action
+    authorize!(:edit, entry)
+  end
+end
+
+class Participation
+  def initialize(person)
+      @person = person
+  end
+
+  def person
+    @person
+  end
 end
   
---
+++ app/domain/rdp_europeanjamboree/export/pdf/registration.rb
@@ -0,0 +1,104 @@
+module RdpEuropeanjamboree
+  module Export::Pdf
+    module Registration
+      class Runner
+        def render(person, pdf_preview)
+      	   return new_pdf(person, pdf_preview).render
+        end
+
+        def new_pdf(person, pdf_preview)
+          pdf = Prawn::Document.new(page_size: 'A4',
+                                    page_layout: :portrait,
+                                    margin: 2.cm,
+                                    bottom_margin: 1.cm)
+          
+          #define header & footer variables
+          imagePath = "../hitobito_rdp_europeanjamboree/app/assets/images/"
+
+          pdf.y = 850
+          #pdf.page_count = 0
+          pdf.repeat :all do
+            #define header
+            if pdf_preview
+              pdf.bounding_box [0,750], :width => pdf.bounds.width, :height => 200 do
+                pdf.transparent(0.5) do
+                  pdf.text "Vorschau:", :size => 24
+                  pdf.text "Nicht zum Versand gedacht!", :size => 24
+                end
+              end
+            end
+
+            logo =   Rails.root.join(imagePath + "ej-logo.png")
+            pdf.bounding_box [350,750], :width => pdf.bounds.width, :height => 375 do
+              pdf.image logo, :width => 150
+              # pdf.move_up 15
+            end
+            
+            footer =   Rails.root.join(imagePath + "ej-footer.png")
+            pdf.bounding_box [150,370], :width => pdf.bounds.width, :height => 604 do
+              pdf.image footer, :width => 496
+            end
+
+            polen =   Rails.root.join(imagePath + "ej-polen.png")
+            pdf.bounding_box [0,pdf.bounds.bottom + 80], :width => pdf.bounds.width, :height => 275 do
+              pdf.image polen, :width => 120
+            end
+
+            rdp =   Rails.root.join(imagePath + "rdp.png")
+            pdf.bounding_box [350,pdf.bounds.bottom + 80], :width => pdf.bounds.width, :height => 275 do
+              pdf.image rdp, :width => 120
+            end
+            
+            pdf.bounding_box [135, pdf.bounds.bottom + 80], :width => pdf.bounds.width do
+              pdf.text "Ring deutscher", :size => 8
+              pdf.text "Pfadfinderinnen- und", :size => 8
+              pdf.text "Pfadfinderverbände e.V.", :size => 8
+              pdf.text "Chausseestraße 128/129", :size => 8
+              pdf.text "10115 Berlin", :size => 8
+            end
+
+            pdf.bounding_box [235, pdf.bounds.bottom + 75], :width => pdf.bounds.width do
+              pdf.text "info@europeanjamboree.de", :size => 8
+              pdf.text "www.europeanjamboree.de", :size => 8
+              pdf.text "Kontingentsleitung:", :size => 8
+              pdf.text "Simone Voit", :size => 8
+              pdf.text "Günther Bäte", :size => 8
+              pdf.text "Thomas Kramer", :size => 8
+            end
+
+            pdf.bounding_box [pdf.bounds.left, pdf.bounds.bottom + 10], :width => pdf.bounds.width do
+              pdf.text person.id.to_s + " - " + person.full_name, :size => 8
+            end
+          end
+
+          pdf.number_pages "S. <page> von <total>",
+            :at => [pdf.bounds.right - 55, pdf.bounds.bottom + 10 ], :size => 8
+          return pdf
+        end
+
+        def customize(pdf)
+          pdf.font_size 9
+          pdf
+        end
+
+        # def sections_TN
+        #     [FrontPage, Package, Signature, AGB ]
+	      # end
+        def sections_else
+            [FrontPage, Package, Signature, AGB]
+        end
+      end
+      mattr_accessor :runner
+      self.runner = Runner
+
+      def self.render(person, pdf_preview)
+        runner.new.render(person, pdf_preview)
+      end
+
+      def self.new_pdf(person, pdf_preview)
+        runner.new.new_pdf(person, pdf_preview)
+      end
+
+    end
+  end
+end
